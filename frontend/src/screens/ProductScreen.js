016256a92b6930fd4d1cbd67a60de1e4bc2f14ef
Uvindu Gunasekara <harshanuv@gmail.com>
2022-07-31 14:30:23 +0530

    connecting with the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 
 function ProductScreen() {
     const params = useParams(); // using useParams Hook
+    // Returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>.
     const { slug } = params;
 
     return (
